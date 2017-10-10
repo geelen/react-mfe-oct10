2b4f715c2d3033e8b212dc3efe2c221224924b5c
Glen Maddern <glenmaddern@gmail.com>
2017-10-10 11:34:08 +1100

    showing setState and props passing

--- src/App.js
+++ src/App.js
@@ -2,40 +2,51 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const AppHeader = () => (
+const AppHeader = ({ greeting }) => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo"/>
-    <h1 className="App-title">Welcome to React</h1>
+    <h1 className="App-title">{ greeting }</h1>
   </header>
 )
 
-const AppIntro = () => (
-  <p className="App-intro">
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
-)
+const AppIntro = () => {
+  console.log("Rendered AppIntro")
+  return (
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  )
+}
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = { foo: 'bar' }
+  state = {
+    greeting: "Hi, Envato Class of 10th October!",
   }
 
   handleClick = () => {
     console.log("clicked!")
-    if (this.state.foo === 'bar') {
-      this.setState({ foo: 'baz!!!!' })
+    if (this.state.greeting.endsWith('!!!!')) {
+      this.setState({ greeting: "Hi, Envato Class of 10th October!", })
     } else {
-      this.setState({ foo: 'bar' })
+      this.setState({ greeting: this.state.greeting + '!' })
     }
   }
 
+  componentDidMount() {
+    // First time that the DOM is live
+  }
+
+  componentWillUnmount() {
+    // Last instant that the DOM is live
+  }
+
   render() {
+    console.log("Rendered!")
     return (
       <div className="App">
-        <AppHeader/>
+        <AppHeader greeting={ this.state.greeting }/>
         <AppIntro/>
-        <p onClick={this.handleClick}>{this.state.foo}</p>
+        <p onClick={this.handleClick}>Click here!!</p>
       </div>
     )
   }
