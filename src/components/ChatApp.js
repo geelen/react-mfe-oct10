c8181bd5b23702aec67b6d06214c07a5f131c6e5
Glen Maddern <glenmaddern@gmail.com>
2017-10-10 14:46:40 +1100

    CHART RE USE

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -2,16 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import ChatInput from './ChatInput'
 
-export const inputHeight = 64
+export const inputHeight = 4
 const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
   > :nth-child(1) {
-    height: calc(100vh - ${inputHeight}px);
+    height: calc(100vh - ${inputHeight}rem);
   }
   > :nth-child(2) {
-    height: ${inputHeight}px;
+    height: ${inputHeight}rem;
   }
 `
 const ChatOutput = styled.div`
--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -2,12 +2,55 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  background: papayawhip;
+  background: #444;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+`
+
+const Input = styled.div`
+  flex-grow: 1;
+  > input {
+    width: calc(100% - 2rem);
+    font-size: 1.25rem;
+    padding: 0.4rem 0.5rem;
+    border: 1px solid #666;
+    border-radius: 0.5rem;    
+    color: chartreuse;
+    background: #333;
+    &::-moz-selection {
+      background: chartreuse;
+      color: black;
+    }
+  }
+`
+const SubmitButton = styled.div`
+  > button {
+    cursor: pointer;
+    background: chartreuse;
+    color: black;
+    font-size: 1.5rem;
+    border: none;
+    height: 2.5rem;
+    width: 5rem;
+    border-radius: 0.5rem;
+    > span {
+      display: block;
+      transform: rotate(-90deg);
+    }
+  }
 `
 
 const ChatInput = () => (
   <Wrapper>
-    Input goes here...
+    <Input>
+      <input type="text"/>
+    </Input>
+    <SubmitButton>
+      <button type="submit" onClick={ console.log("Clicked!") }>
+        <span>➡</span>
+      </button>
+    </SubmitButton>
   </Wrapper>
 )
 
